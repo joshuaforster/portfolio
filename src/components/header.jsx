953b6c96c923418fa523fcc5d4edf88c3e58b7eb
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-03-23 09:57:06 +0000

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,23 +9,10 @@ export default function Header() {
                     <img src="/joshLogo.png" alt="Josh's Logo" className="h-10"/>
                 </Link>
                 <nav className="flex space-x-4">
-                    {/* Scroll to the home section */}
-                    <Link to='/' className="text-lg font-medium text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
-                        <a href='hero'> Home</a>
+                    <Link href="#hero" className="text-lg font-medium text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                        Home
                     </Link>
-                    {/* Scroll to the about section */}
-                    <a href="#about" className="text-lg font-medium text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
-                        About
-                    </a>
-                    {/* Scroll to the portfolio section */}
-                    <a href="#portfolio" className="text-lg font-medium text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
-                        Portfolio
-                    </a>
-                    {/* Scroll to the contact section */}
-                    <a href="#contact" className="text-lg font-medium text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
-                        Contact
-                    </a>
-                    {/* Navigate to the blog page */}
+                
                     <Link to="/blog" className="text-lg font-medium text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
                         Blog
                     </Link>
