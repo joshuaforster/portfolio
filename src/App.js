988ec3f7eeaa74425adfb0e7e9494bc84f03b511
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-06-20 16:42:54 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home"
 import PortfolioDetail from "./pages/PortfolioDetail";
 import Layout from "./components/layout"
-import BlogPage from "./pages/blog";
-import BlogDetail from "./pages/blogDetail";
+// import BlogPage from "./pages/blog";
+// import BlogDetail from "./pages/blogDetail";
 import NothingHere from "./pages/nothingHere";
 import './App.css'
 
@@ -15,8 +15,8 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />}/>
-          <Route path='/blog' element={<BlogPage />} />
-          <Route path="/blog/:id" element={<BlogDetail />} /> {/* New route for individual blog posts */}
+          {/* <Route path='/blog' element={<BlogPage />} /> */}
+          {/* <Route path="/blog/:id" element={<BlogDetail />} /> New route for individual blog posts */}
           <Route path="/portfolio/:id" element={<PortfolioDetail/>} />
           <Route path='*'element={<NothingHere/>} />
         </Route>
